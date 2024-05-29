5b371f19ecc2601b9774b511a11d33313969a987
Nikhil <kharb.nikhil10@gmail.com>
2024-05-29 21:19:14 +0530

    QA commit

---
+++ src/C2.java
@@ -0,0 +1,25 @@
+import java.time.Duration;
+import java.util.ArrayList;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class C2 {
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("http://180.151.246.51:8087/");
+		Thread.sleep(500);
+		driver.findElement(By.id("mat-input-0")).sendKeys("admin");
+		driver.findElement(By.name("password")).sendKeys("Bci@123456");
+		driver.findElement(
+				By.xpath("/html/body/app-root/ng-component/div/ng-component/div/div/form/div[3]/div[1]/button"))
+				.click();
+		Thread.sleep(1000);
+		driver.navigate().to("http://180.151.246.51:8087/app/createforms/1235");
+		ArrayList<String> pallet = new ArrayList<String>();
+	}
+}
